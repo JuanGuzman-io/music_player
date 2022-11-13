aa80f9b3d8e14a1b6e5677e9d0b4daa6f605bcb1
Juan Delgado <juandelgadog98@gmail.com>
2022-11-13 12:49:45 -0500

    :sparkles: added

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+const pool = new Pool(!process.env.DEV_MODE ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } } : null);
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
